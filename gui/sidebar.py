daeb161c49b3a4380c8f070add79d3c3dbcc136d
iczajowska <izabelaczajowska@gmail.com>
2022-05-20 19:19:53 +0200

    sliders configuration - live simulation update

--- gui/sidebar.py
+++ gui/sidebar.py
@@ -2,6 +2,8 @@ import pygame
 from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 
+from gui.slider_type import SliderType
+
 
 class SideBar:
 
@@ -11,7 +13,7 @@ class SideBar:
         self.items = {}
         self.font = pygame.font.SysFont('georgia', 12)
 
-    def add_slider(self, surface, offset_y, text, minimum, maximum, initial):
+    def add_slider(self, surface, offset_y, text, minimum, maximum, initial, slider_type: SliderType):
         text_surface = self.font.render(text, True, (0, 0, 0))
         surface.blit(text_surface, (self.X, self.Y+offset_y))
         slider = Slider(surface, self.X, self.Y + offset_y + 25, 90, 10, min=minimum, max=maximum, initial=initial,
@@ -19,8 +21,11 @@ class SideBar:
         output = TextBox(surface, self.X+30, self.Y + offset_y + 40, 30, 20, fontSize=16, borderThickness=0, radius=1,
                          font=self.font, colour=(255, 217, 179))
         output.disable()
-        self.items[slider] = output
+        self.items[slider_type] = (slider, output)
 
     def update_sliders(self):
-        for key, value in self.items.items():
-            value.setText(key.getValue())
+        for (slider, text_box) in self.items.values():
+            text_box.setText(slider.getValue())
+
+    def get_slider_by_type(self, slider_type: SliderType):
+        return self.items[slider_type][0]
---
+++ gui/simulation_settings.py
@@ -0,0 +1,34 @@
+import pygame_widgets
+from pygame import Surface
+
+from config import PLANT_ENERGY, PLANT_GROWTH_PER_ROUND, BREED_ENERGY_FEMALE, BREED_ENERGY_MALE, \
+    ENERGY_DEMAND_PER_ROUND, NUMBER_OF_MALE_ANIMALS, NUMBER_OF_FEMALE_ANIMALS
+from gui.slider_type import SliderType
+from gui.sidebar import SideBar
+
+
+class SimulationSettings:
+    side_bar: SideBar
+    screen: Surface
+
+    def __init__(self, side_bar: SideBar, screen: Surface):
+        self.side_bar = side_bar
+        self.screen = screen
+
+    def set_default_configuration(self):
+        self.side_bar.add_slider(self.screen, 0, "Number of males", 1, 20, NUMBER_OF_MALE_ANIMALS, SliderType.MALE)
+        self.side_bar.add_slider(self.screen, 70, "Number of females", 1, 20, NUMBER_OF_FEMALE_ANIMALS, SliderType.FEMALE)
+        self.side_bar.add_slider(self.screen, 140, "Plant growth per round", 1, 100, PLANT_GROWTH_PER_ROUND, SliderType.PL_PER_ROUND)
+        self.side_bar.add_slider(self.screen, 210, "Plant energy value", 1, 100, PLANT_ENERGY, SliderType.PL_ENERGY)
+        self.side_bar.add_slider(self.screen, 280, "Animal energy demand per day", 1, 20, ENERGY_DEMAND_PER_ROUND, SliderType.A_DAY_ENERGY)
+        self.side_bar.add_slider(self.screen, 350, "Breeding energy demand - female", 1, 90, BREED_ENERGY_FEMALE, SliderType.F_BREEDING)
+        self.side_bar.add_slider(self.screen, 420, "Breeding energy demand - male", 1, 90, BREED_ENERGY_MALE, SliderType.M_BREEDING)
+        self.side_bar.add_slider(self.screen, 490, "Maximum animal age - in rounds", 1, 300, 100, SliderType.MAX_AGE)
+
+    def update_settings(self, events):
+        self.side_bar.update_sliders()
+        pygame_widgets.update(events)
+
+    def get_slider_value_by_type(self, slider_type: SliderType):
+        slider = self.side_bar.get_slider_by_type(slider_type)
+        return slider.getValue()
